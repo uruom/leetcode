4008b2ec680aba345154e7a9744a905441508541
uruom <71624685+uruom@users.noreply.github.com>
2021-11-24 19:30:29 +0800

    Create 278.cpp

---
+++ Binary Search/278.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long l=1,r=n,mid=(l+r)/2;
+        while(l!=r)
+        {
+            if(isBadVersion(mid))
+                r=mid;
+            else 
+                l=mid+1;
+            mid=(l+r)/2;
+        }
+        return l;
+    }
+};
